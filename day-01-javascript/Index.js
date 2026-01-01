b931c5af63e9a2a91dd911005a5146b35f903e8c
Abdul hadi <a.hadi4420386@gmail.com>
2026-01-01 23:51:39 +0500

    30/1 Day 1: JavaScript basics refreshed

---
+++ day-01-javascript/Index.js
@@ -0,0 +1,33 @@
+// Day 1 - JavaScript Basics
+
+// Variables
+
+let age = 24;
+const name = "Abdul Hadi";
+
+age = 25 // ✅ allowed
+name = "Ali"; // ❌ error (const)
+
+// Arrow Functions
+
+const greet = (names) => {
+  return `Hello, ${names}`;
+};
+
+console.log(greet(names));
+
+// Array + Loop
+let skills = ["JavaScript", "Next.js", "Python"];
+
+skills.map(skill => {
+  console.log("Skill:", skill);
+});
+
+// Object
+let user = {
+  name: name,
+  age: age,
+  skills: skills
+};
+
+console.log(user);
